aadb377134858cb90c7fbadbbf8e5c273139ef0c
Cyprian Tinashe Aarons <cyprianaarons263@gmail.com>
2023-04-17 15:42:24 +0200

    update

--- api/http-common.js
+++ api/http-common.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
+// const isProduction = process.env.NODE_ENV === "production";
 const isProduction = true;
 
 export default axios.create({
   baseURL: isProduction
-    ? "https://new-coindraw-api.onrender.com/api"
+    ? "https://dev-coindraw-api.onrender.com/api"
     : "http://localhost:8080/api",
   headers: {
     "Content-type": "application/json",
