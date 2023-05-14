95d2169894d307f5858e53191051b101d0ea8e4c
Cyprian Tinashe Aarons <cyprianaarons263@gmail.com>
2023-05-14 14:46:37 +0200

    update

--- common/addresses.js
+++ common/addresses.js
@@ -1,6 +1,6 @@
 //Main Site
-const distributeAddress = "0xFcf2c6c93bB4fD115c696dF358cd067F063d2c6D";
-const airdropAddress = "0xdB9f2b43a75fb1C9bcBFC56BC03ED22763B1A5d3";
+const distributeAddress = "0x36C097adbb97F8cb691A4F3a336f06B3De6C6604";
+const airdropAddress = "0x796Bd40DB0Ac550c7bF456DC4bdBf910591221Aa";
 const referralAddress = "0x3c5aBdCfBb6B9cB8cD9B43Ff4f66DfC6Df1d9eC9";
 const currentClassicAddress = "0x767aDae846d75d5Ef9937151c7e4aC71Bb96D4fb";
 
