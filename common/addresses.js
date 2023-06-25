5b60b2c76e7c959c9f877d58cdbaa7ebc0f964c8
Cyprian Tinashe Aarons <cyprianaarons263@gmail.com>
2023-06-25 23:11:14 +0200

    update

--- common/addresses.js
+++ common/addresses.js
@@ -1,5 +1,5 @@
 //Main Site
-const distributeAddress = "0x36C097adbb97F8cb691A4F3a336f06B3De6C6604";
+const distributeAddress = "0xc5AEaFEd26c2D3fc5Ae6b66da0D5e9c7D17b3Ef7";
 const airdropAddress = "0x796Bd40DB0Ac550c7bF456DC4bdBf910591221Aa";
 const referralAddress = "0x3c5aBdCfBb6B9cB8cD9B43Ff4f66DfC6Df1d9eC9";
 const currentClassicAddress = "0x767aDae846d75d5Ef9937151c7e4aC71Bb96D4fb";
