a9e1a3c0b8bb5f4b0bcb9eec79317edc37463604
Cyprian Tinashe Aarons <cyprianaarons263@gmail.com>
2024-04-03 15:02:38 +0200

    update

--- api/http-common.js
+++ api/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const isProduction = process.env.NODE_ENV !== "production";
+const isProduction = process.env.NODE_ENV === "production";
 
 const instance = axios.create({
   baseURL: isProduction
